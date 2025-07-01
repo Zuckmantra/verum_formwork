793ff9ed6a68bee7b64dd72edf69c7ba711191c8
Zuckmantra <haroldjul15@gmail.com>
2025-07-01 15:25:01 -0500

    Diseño responsive para pagina principal

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let carruselImages = document.querySelectorAll('#carrusel_images img');
+let mainImage = document.querySelector('.visor img');
+let leftArrow = document.querySelector('#left-arrow');
+let rightArrow = document.querySelector('#right-arrow');
+let currentIndex = 0;
+
+rightArrow.addEventListener('click', () => {
+    currentIndex++;
+    if (currentIndex >= carruselImages.length) {
+        currentIndex = 0;
+    }
+    updateMainImage();
+});
+
+leftArrow.addEventListener('click', () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = carruselImages.length - 1;
+    }
+    updateMainImage();
+});
+
+function updateMainImage() {
+    mainImage.src = carruselImages[currentIndex].src;
+}
